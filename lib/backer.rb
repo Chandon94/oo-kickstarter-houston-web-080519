7234bbd76298dc44e8039af9053b5c13db54e3df
Chandon Porter <cneilporter94@gmail.com>
2019-08-12 19:11:11 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+
+
+class Backer < Project
+  attr_reader :name, :backed_projects
+  
+  
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+    
+  end
+  
+  def back_projects(project)
+    backed.projects << self
+  end
+  
+end
